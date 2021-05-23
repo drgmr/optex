7f85acd594af7aef2c2127a6423d6ea983ce00b2
Eduardo Cunha <hey@drgmr.com>
2021-05-23 14:11:45 -0300

    Replace Main with a phonebook args example

--- src/Main.hs
+++ src/Main.hs
@@ -1,43 +1,49 @@
-{-# LANGUAGE NamedFieldPuns #-}
-
 module Main where
 
-import Data.Char (toUpper)
 import Options.Applicative
-  ( argument,
-    execParser,
-    help,
-    info,
-    long,
-    metavar,
-    short,
-    str,
-    switch,
-  )
 
-data Welcome = Welcome
-  { name :: String,
-    excited :: Bool
-  }
+data Command
+  = Add String String
+  | Phone String String
+  | Email String String
+  | Show String
+  deriving (Show, Eq)
 
-runWithOptions :: Welcome -> IO ()
-runWithOptions Welcome {name, excited} =
-  putStrLn $ transform ("Enjoy the snow, " ++ name ++ "!")
-  where
-    transform =
-      if excited
-        then map toUpper
-        else id
+parserAdd :: Parser Command
+parserAdd =
+  Add
+    <$> strArgument (metavar "FILENAME")
+    <*> strArgument (metavar "PERSON_NAME")
+
+parserPhone :: Parser Command
+parserPhone =
+  Phone
+    <$> strArgument (metavar "FILENAME")
+    <*> strArgument (metavar "PHONE")
+
+parserEmail :: Parser Command
+parserEmail =
+  Email
+    <$> strArgument (metavar "FILENAME")
+    <*> strArgument (metavar "EMAIL")
+
+parserShow :: Parser Command
+parserShow =
+  Show
+    <$> strArgument (metavar "FILENAME")
+
+parser :: Parser Command
+parser =
+  subparser $
+    command "add" (parserAdd `withInfo` "Add a new entry")
+      <> command "phone" (parserPhone `withInfo` "Add a phone to an existing entry")
+      <> command "email" (parserEmail `withInfo` "Add an email to an existing entry")
+      <> command "show" (parserShow `withInfo` "Displays an existing entry")
+
+withInfo :: Parser a -> String -> ParserInfo a
+withInfo parser desc = info (helper <*> parser) $ progDesc desc
 
 main :: IO ()
-main = execParser opts >>= runWithOptions
-  where
-    parser =
-      Welcome
-        <$> argument str (metavar "NAME")
-        <*> switch
-          ( short 'e'
-              <> long "excited"
-              <> help "Run in excited mode."
-          )
-    opts = info parser mempty
+main = do
+  command <- execParser $ parser `withInfo` "Manage the address book"
+  print command
